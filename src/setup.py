767953567c2514b5e9caffc352873a9cb7d9e9b1
paloblanco <45581243+paloblanco@users.noreply.github.com>
2022-05-25 20:58:56 -0400

    local pip install working

---
+++ src/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(  name='simplepysite',
+        version='0.1',
+        description='Simple static site generator',
+        packages=['simplepysite'],
+        zip_safe=False)
